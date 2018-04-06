a7c5d9833ed7f330ee13eb7bf54e0c38bcbda3b7
abhirath15 <abhirath.seth15@gmail.com>
2018-04-06 16:34:56 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const os = require('os');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+
+
+app.use((req,res,next)=>{
+	res.render('demo.hbs');
+		
+	console.log('inside demo');
+	
+	// next();
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=>{
+	// res.send('hello Express');
+	// res.send({
+	// 	name: 'abhirath',
+	// 	likes: [
+	// 	'beer',
+	// 	'adventure'
+	// 	]
+	// });
+	res.render('home.hbs',{
+		title: 'Home Page',
+		message: "welcome",
+		currentyear: new Date()
+	});
+
+});
+
+
+
+hbs.registerHelper('screamIt', (text)=>{
+	return text.toUpperCase();
+
+})
+
+app.get('/bad',(req,res)=>{
+	res.send({
+		ErrorMessage: 'error 404'
+	});
+});
+
+app.listen(3000);
